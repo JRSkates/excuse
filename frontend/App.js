ac24e5a05b825a8225a079c81f2243240b953495
Sharmine <sharmine.ss@gmail.com>
2023-07-06 15:19:28 +0100

    video plays when clicking Generate excuse

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImageBackground, StyleSheet, View, StatusBar } from "react-native";
 import { NativeBaseProvider, Text, Button } from "native-base";
+import { Video } from 'expo-av';
 
 export default function App() {
+  const [isVideoPlaying, setVideoPlaying] = useState(false);
+
+  const handleButtonPress = () => {
+    setVideoPlaying(true);
+  };
+
   return (
     <NativeBaseProvider>
-      <ImageBackground
-        source={require('./assets/background.png')}
-        style={styles.backgroundImage}
+      {!isVideoPlaying ? (
+        <ImageBackground
+          source={require('./assets/background.png')}
+          style={styles.backgroundImage}
         >
-          <StatusBar backgroundColor='black' barStyle="light-content"/>
+          <StatusBar backgroundColor="black" barStyle="light-content" />
           <View style={styles.container}>
-              <Button>Generate excuse</Button>
+            <Button onPress={handleButtonPress}>Generate excuse</Button>
           </View>
-      </ImageBackground>
+        </ImageBackground>
+      ) : (
+        <View style={styles.videoContainer}>
+          <Video
+            source={require('./assets/loading.mp4')} // Replace with the path to your video file
+            style={styles.video}
+            resizeMode="cover"
+            isLooping
+            shouldPlay
+          />
+        </View>
+      )}
     </NativeBaseProvider>
   );
 }
@@ -27,5 +46,11 @@ const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
     resizeMode: 'cover',
-  }
+  },
+  videoContainer: {
+    flex: 1,
+  },
+  video: {
+    flex: 1,
+  },
 });
