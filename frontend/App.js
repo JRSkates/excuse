1835c3c8600cf8cd790f8fe0523b1121d97a10e9
Sharmine <sharmine.ss@gmail.com>
2023-07-06 10:46:31 +0100

    installed component library but things aren't working

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Excuse</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ImageBackground 
+        source={require('./assets/background.jpg')}
+        style={styles.backgroundImage}
+        >
+        <View style={styles.container}>
+          <Text style={styles.text}>Excuse</Text>
+          <StatusBar backgroundColor="white" barStyle="dark-content" /> 
+        <View>
+          <Button title="Generate excuse" />
+        </View>
+        </View>
+      </ImageBackground>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',    
+  },
+  text: {
+    color: 'white'
+  }
 });
+
+
+
