fb78450f797b7cabbcaf423118c1e0fd7618473e
George Barrett <georgeaustinbarrett@gmail.com>
2023-07-07 15:47:54 +0100

    logic for handling errors from the api implemented. test written. running GREEN

--- api/spec/controllers/excuseGenerator.spec.js
+++ api/spec/controllers/excuseGenerator.spec.js
@@ -6,23 +6,24 @@ const ExcuseController = require('../../controllers/excuseGenerator');
 
 jest.mock('openai', () => ({
   Configuration: jest.fn(),
-  OpenAIApi: jest.fn(() => ({
-    createChatCompletion: jest.fn().mockResolvedValueOnce({
-      data: {
-        choices: [
-          {
-            message: {
-              content: 'I am late'
-            }
-          }
-        ]
-      }
-    })
-  }))
+  OpenAIApi: jest.fn(),
 }));
 
 describe('GET /excuse', () => {
   it('should return an excuse', async () => {
+    OpenAIApi.mockImplementationOnce(() => ({
+        createChatCompletion: jest.fn().mockResolvedValueOnce({
+          data: {
+            choices: [
+              {
+                message: {
+                  content: 'I am late'
+                }
+              }
+            ]
+          }
+        })
+      }));
     const response = await request(app)
       .get('/excuse');
 
@@ -30,4 +31,15 @@ describe('GET /excuse', () => {
     expect(response.body).toEqual({ excuse: 'I am late' });
     //expect(ExcuseController.generateExcuse).toHaveBeenCalledTimes(1);
   });
+
+  it('should handle errors and return an error response', async () => {
+    OpenAIApi.mockImplementationOnce(() => ({
+      createChatCompletion: jest.fn().mockRejectedValueOnce(new Error('Failed to generate excuse.'))
+    }))
+    const response = await request(app)
+      .get('/excuse');
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({error: 'Failed to generate excuse.'})
+  })
 });
