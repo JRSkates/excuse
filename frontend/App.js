06e0f79bf4bd1e0bab5c0dd4e017459c9fcd9c19
sharmine-s <sharmine.ss@gmail.com>
2023-07-07 10:32:00 +0100

    fixed axios network error and created .env in frontend where IP will live

--- frontend/App.js
+++ frontend/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { ImageBackground, StyleSheet, View, StatusBar } from "react-native";
 import { NativeBaseProvider, Text, Button } from "native-base";
 import axios from 'axios';
+import { IP } from '@env';
 
 export default function App() {
   const [excuse, setExcuse] = useState('')
 
   const generateExcuse = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/excuse')
+      const response = await axios.get(`${IP}/excuse`)
       const data = response.data;
       console.log(response)
 
