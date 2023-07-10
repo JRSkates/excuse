ae2ac038f20ddf254b7b573be63ddcd2dd26cb8d
awdem <wdemontmollin@gmail.com>
2023-07-10 14:21:19 +0100

    feat: changes openAI prompt to handle optional event description

--- api/controllers/excuseGenerator.js
+++ api/controllers/excuseGenerator.js
@@ -7,11 +7,18 @@ const ExcuseController = {
       const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY
     });
-  
+
     const openai = new OpenAIApi(configuration);
     const chatCompletion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: `Can you return the following phrase: ${req.query}` }]
+      messages: [
+        {role: 'system', content: `
+        You will receive a description an event.
+        Return a fake excuse that no one would believe that the describer of the event can use to get out of it. Make it funny.
+        If you do not receive a description, please return an excuse with the same constraints which can be used for any event.
+        `},
+        { role: 'user', content: `${req.query.eventType}` },
+      ]
     });
     
     console.log(chatCompletion.data.choices[0].message.content);
