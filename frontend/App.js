30bd6ef3c1343614dfcb063b63d38de3768f6989
George Barrett <georgeaustinbarrett@gmail.com>
2023-07-10 13:35:19 +0100

    initial styling for share button complete

--- frontend/App.js
+++ frontend/App.js
@@ -43,7 +43,7 @@ export default function App() {
             <Text style={styles.excuseText}>{excuse}</Text>
           </View>
           {excuseGenerated && (
-          <View>
+          <View style={styles.container}>
             <Button onPress={shareExcuse}>
               Share
             </Button>
