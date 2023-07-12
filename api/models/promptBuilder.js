81339c88565b97b1192c3567522bf8caec2cd468
awdem <wdemontmollin@gmail.com>
2023-07-12 13:46:47 +0100

    feat: incorporates excuseType from user input to construct the prompt

--- api/models/promptBuilder.js
+++ api/models/promptBuilder.js
@@ -1,11 +1,26 @@
 class PromptBuilder {
-  constructPrompt() {
-    const systemContent =  `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions: 
+  constructPrompt(excuseType = null) {
+    let systemContent =  `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions: 
     - Provide an example of a funny, creative and outlandish excuse to not attend something. This excuse should be up to 5 lines long. Don't put it in quotations. Make sure the responses are in the First Person ("I can't make it")`;
-    const prompt = [{
+    
+    let prompt = [{
       role: 'system',
       content: systemContent
     }];
+
+    if (excuseType ) {
+      systemContent = systemContent + `\n    - As a User input, you will receive the name or description of the event someone is trying to get out of.  Make sure to incorporate this input into the excuse.`
+      prompt = [
+        {
+          role: 'system',
+          content: systemContent
+        }, 
+        {
+          role: 'user',
+          content: excuseType
+        }
+      ];
+    }
 
     return prompt;
   }
--- api/spec/models/promptBuilder.test.js
+++ api/spec/models/promptBuilder.test.js
@@ -12,4 +12,21 @@ describe('Prompt Builder class', () => {
       }
     ])
   })
+
+  it('given an event description, returns the prompt with event description', () => {
+    const builder = new PromptBuilder;
+    const systemContent =  `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions: 
+    - Provide an example of a funny, creative and outlandish excuse to not attend something. This excuse should be up to 5 lines long. Don't put it in quotations. Make sure the responses are in the First Person ("I can't make it")
+    - As a User input, you will receive the name or description of the event someone is trying to get out of.  Make sure to incorporate this input into the excuse.`
+    expect(builder.constructPrompt('my sons birthday')).toEqual([
+      {
+        role: 'system',
+        content: systemContent
+      },
+      { 
+        role: 'user',
+        content: 'my sons birthday'
+      }
+    ])
+  })
 });
