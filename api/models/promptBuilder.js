1905bad98ebf99b5494b57f305ce863eed986580
awdem <wdemontmollin@gmail.com>
2023-07-12 13:24:11 +0100

    feat: promptBuilder returns basic prompt

--- api/models/promptBuilder.js
+++ api/models/promptBuilder.js
@@ -1,5 +1,14 @@
 class PromptBuilder {
+  constructPrompt() {
+    const systemContent =  `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions: 
+    - Provide an example of a funny, creative and outlandish excuse to not attend something. This excuse should be up to 5 lines long. Don't put it in quotations. Make sure the responses are in the First Person ("I can't make it")`;
+    const prompt = [{
+      role: 'system',
+      content: systemContent
+    }];
 
+    return prompt;
+  }
 }
 
 module.exports = PromptBuilder;
