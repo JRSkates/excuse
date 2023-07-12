d93cd77bbcb8166fdf354e7e28b91e67931c5d62
awdem <wdemontmollin@gmail.com>
2023-07-12 13:11:28 +0100

    test: fixes assertion in first test

--- api/spec/models/promptBuilder.test.js
+++ api/spec/models/promptBuilder.test.js
@@ -3,7 +3,13 @@ const PromptBuilder = require("../../models/promptBuilder.js")
 describe('Prompt Builder class', () => {
   it('given no optional inputs, returns the basic prompt', () => {
     const builder = new PromptBuilder;
-
-    expect(builder.constructPrompt()).toEqual([])
+    const systemContent =  `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions: 
+    - Provide an example of a funny, creative and outlandish excuse to not attend something. This excuse should be up to 5 lines long. Don't put it in quotations. Make sure the responses are in the First Person ("I can't make it")`
+    expect(builder.constructPrompt()).toEqual([
+      {
+        role: 'system',
+        content: systemContent
+      }
+    ])
   })
 });
