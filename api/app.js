456f03f72250c7949f33c0bdbd6f5e6d2501c278
JRSkates <skatesproduction@gmail.com>
2023-07-13 16:19:48 +0100

    Prettier VSCode Refactoring and Formatting

--- api/app.js
+++ api/app.js
@@ -1,25 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var excuseRouter = require('./routes/excuse');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var excuseRouter = require("./routes/excuse");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/excuse', excuseRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/excuse", excuseRouter);
 
 module.exports = app;
--- frontend/App.test.js
+++ frontend/App.test.js
@@ -1,17 +1,16 @@
 import App from "./App";
 import React from "react";
-import { jest } from '@jest/globals';
-import { Share, SwitchChangeEvent} from "react-native";
+import { jest } from "@jest/globals";
+import { Share, SwitchChangeEvent } from "react-native";
 import {
   render,
   fireEvent,
   screen,
-  waitFor
+  waitFor,
 } from "@testing-library/react-native";
 import axios from "axios";
 
 jest.mock("axios");
-
 
 describe("App", () => {
   it("should return an excuse to the frontend when the button is pressed", async () => {
@@ -25,19 +24,27 @@ describe("App", () => {
 
     const excuseText = await waitFor(() => screen.getByText("I am late"));
     expect(excuseText).toBeTruthy();
-  })
+  });
 
-  it('should return a message if there is an API error', async () => {
-    axios.get.mockRejectedValueOnce(new Error('Excuse Generator is sick of your lies right now, try again shortly...'));
+  it("should return a message if there is an API error", async () => {
+    axios.get.mockRejectedValueOnce(
+      new Error(
+        "Excuse Generator is sick of your lies right now, try again shortly..."
+      )
+    );
 
     render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
-    
-    fireEvent.press(generateExcuseButton)
+    const generateExcuseButton = screen.getByText("Generate Excuse");
 
-    const errorMessage = await waitFor(() => screen.getByText('Excuse Generator is sick of your lies right now, try again shortly...'));
+    fireEvent.press(generateExcuseButton);
+
+    const errorMessage = await waitFor(() =>
+      screen.getByText(
+        "Excuse Generator is sick of your lies right now, try again shortly..."
+      )
+    );
     expect(errorMessage).toBeTruthy();
-  })
+  });
 
   it("should have an input field", () => {
     render(<App />);
@@ -52,16 +59,19 @@ describe("App", () => {
     const inputField = screen.getByLabelText("Excuse type input field");
     const generateExcuseButton = screen.getByText("Generate Excuse");
     // inputs text
-    fireEvent.changeText(inputField, 'my sons birthday');
+    fireEvent.changeText(inputField, "my sons birthday");
     // presses button
     fireEvent.press(generateExcuseButton);
 
-    expect(axios.get).toHaveBeenLastCalledWith('https://excuse-s1se.onrender.com/excuse', { 
-      params:{
-        eventType: 'my sons birthday',
-        "toggle": false
+    expect(axios.get).toHaveBeenLastCalledWith(
+      "https://excuse-s1se.onrender.com/excuse",
+      {
+        params: {
+          eventType: "my sons birthday",
+          toggle: false,
+        },
       }
-    })
+    );
   });
 
   it("it clears the input field on button press", async () => {
@@ -70,116 +80,113 @@ describe("App", () => {
     const inputField = screen.getByLabelText("Excuse type input field");
     const generateExcuseButton = screen.getByText("Generate Excuse");
     // inputs text
-    fireEvent.changeText(inputField, 'my sons birthday');
+    fireEvent.changeText(inputField, "my sons birthday");
     // presses button
-    fireEvent.press(generateExcuseButton); 
+    fireEvent.press(generateExcuseButton);
     //console.log(inputField.props);
     await waitFor(() => expect(inputField.props.defaultValue).toBe(""));
-
   });
 
-  it('should display the share button after an excuse is generated', async () => {
-    const mockData = { excuse: "I am late"}
-    axios.get.mockResolvedValue({ data: mockData })
-
-    render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
-    
-    fireEvent.press(generateExcuseButton)
-    await waitFor(() => screen.getByText('I am late'));
-
-    const shareButton = screen.getByText('Share');
-
-    expect(shareButton).toBeTruthy();
-
-  });
-  
-  it('should allow the user to share an excuse', async () => {
+  it("should display the share button after an excuse is generated", async () => {
     const mockData = { excuse: "I am late" };
     axios.get.mockResolvedValue({ data: mockData });
-  
+
     render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
-  
+    const generateExcuseButton = screen.getByText("Generate Excuse");
+
     fireEvent.press(generateExcuseButton);
-    await waitFor(() => screen.getByText('I am late'));
-  
-    const shareButton = screen.getByText('Share');
+    await waitFor(() => screen.getByText("I am late"));
+
+    const shareButton = screen.getByText("Share");
+
+    expect(shareButton).toBeTruthy();
+  });
+
+  it("should allow the user to share an excuse", async () => {
+    const mockData = { excuse: "I am late" };
+    axios.get.mockResolvedValue({ data: mockData });
+
+    render(<App />);
+    const generateExcuseButton = screen.getByText("Generate Excuse");
+
+    fireEvent.press(generateExcuseButton);
+    await waitFor(() => screen.getByText("I am late"));
+
+    const shareButton = screen.getByText("Share");
     //console.log(shareButton);
-  
-    const shareSpy = jest.spyOn(Share, 'share');
-  
+
+    const shareSpy = jest.spyOn(Share, "share");
+
     fireEvent.press(shareButton);
-  
+
     expect(shareSpy).toHaveBeenCalled();
     expect(shareSpy).toHaveBeenCalledWith({
-      message: 'I am late',
+      message: "I am late",
     });
-  
+
     shareSpy.mockRestore();
   });
-  it('on press, button goes into loading state, and after the excuse loads, the original button is displayed again', async () => {
+  it("on press, button goes into loading state, and after the excuse loads, the original button is displayed again", async () => {
     render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
+    const generateExcuseButton = screen.getByText("Generate Excuse");
     fireEvent.press(generateExcuseButton);
 
     expect(screen.getByText("Submitting")).toBeTruthy();
 
-    await waitFor(() => expect(screen.getByText("Generate Excuse")).toBeTruthy());
-  });
-  
-
-  it('should toggle the toggle button', async () => {
-
-    const mockData = { excuse: "I am late"}
-    axios.get.mockResolvedValue({ data: mockData })
-
-    render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
-    
-    fireEvent.press(generateExcuseButton)
-    await waitFor(() => screen.getByText('I am late'));
-
-    const toggleSwitch = screen.getByTestId('switch');
-
-    fireEvent(toggleSwitch, 'onValueChange', true);
-
-    expect(toggleSwitch.props.value).toBe(true);
-    
-  });
-  it('should display the copy button after an excuse is generated', async () => {
-    const mockData = { excuse: "I am late"}
-    axios.get.mockResolvedValue({ data: mockData })
-
-    render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
-    
-    fireEvent.press(generateExcuseButton)
-    await waitFor(() => screen.getByText('I am late'));
-
-    const copyButton = screen.getByText('Copy');
-
-    expect(copyButton).toBeTruthy();
-
+    await waitFor(() =>
+      expect(screen.getByText("Generate Excuse")).toBeTruthy()
+    );
   });
 
-  it('should copy the excuse to clipboard using useClipboard', async () => {
+  it("should toggle the toggle button", async () => {
     const mockData = { excuse: "I am late" };
     axios.get.mockResolvedValue({ data: mockData });
 
     render(<App />);
-    const generateExcuseButton = screen.getByText('Generate Excuse');
-  
+    const generateExcuseButton = screen.getByText("Generate Excuse");
+
     fireEvent.press(generateExcuseButton);
-    await waitFor(() => screen.getByText('I am late'));
-  
-    const copyButton = screen.getByText('Copy');
+    await waitFor(() => screen.getByText("I am late"));
+
+    const toggleSwitch = screen.getByTestId("switch");
+
+    fireEvent(toggleSwitch, "onValueChange", true);
+
+    expect(toggleSwitch.props.value).toBe(true);
+  });
+  it("should display the copy button after an excuse is generated", async () => {
+    const mockData = { excuse: "I am late" };
+    axios.get.mockResolvedValue({ data: mockData });
+
+    render(<App />);
+    const generateExcuseButton = screen.getByText("Generate Excuse");
+
+    fireEvent.press(generateExcuseButton);
+    await waitFor(() => screen.getByText("I am late"));
+
+    const copyButton = screen.getByText("Copy");
+
+    expect(copyButton).toBeTruthy();
+  });
+
+  it("should copy the excuse to clipboard using useClipboard", async () => {
+    const mockData = { excuse: "I am late" };
+    axios.get.mockResolvedValue({ data: mockData });
+
+    render(<App />);
+    const generateExcuseButton = screen.getByText("Generate Excuse");
+
+    fireEvent.press(generateExcuseButton);
+    await waitFor(() => screen.getByText("I am late"));
+
+    const copyButton = screen.getByText("Copy");
 
     fireEvent.press(copyButton);
 
-    expect(screen.getByText('Copied!')).toBeTruthy()
+    expect(screen.getByText("Copied!")).toBeTruthy();
 
-    await waitFor(() => expect(screen.getByText('Copy')).toBeTruthy(), { timeout: 2500 });
-
+    await waitFor(() => expect(screen.getByText("Copy")).toBeTruthy(), {
+      timeout: 2500,
     });
+  });
 });
