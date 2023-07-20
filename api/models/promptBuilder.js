e8b58b9d34666f70dc3035ae8d6068c4c1c0bac7
awdem <wdemontmollin@gmail.com>
2023-07-20 11:57:34 +0100

    tests: updates promptBuilder tests to reflect separation of system and user communication with openAI

--- api/models/promptBuilder.js
+++ api/models/promptBuilder.js
@@ -3,9 +3,7 @@ class PromptBuilder {
     this.prompt = [
       {
         role: "system",
-        content: `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions:
-        - Provide an example of a funny, creative and outlandish excuse to not attend something. This excuse should be up to 5 lines long. Don't put it in quotations. Make sure the responses are in the First Person ("I can't make it")
-        You may receive additional prompts from Users with instructions, Make sure you use all of the User prompts in your response, with the desired behaviour.`
+        content: `Your purpose is to be an Excuse Generator. You should provide an example of a funny, creative and outlandish excuse to not attend something. Please follow the following instructions:\n- Provide an example of a funny, creative and outlandish excuse to not attend something. This excuse should be up to 5 lines long. Don't put it in quotations. Make sure the responses are in the First Person ("I can't make it")\nYou may receive additional prompts from Users with instructions, Make sure you use all of the User prompts in your response, with the desired behaviour.`
       },
     ]
   }
@@ -14,8 +12,7 @@ class PromptBuilder {
     if (excuseType) {
       let excuseTypePrompt = {
         role: "user",
-        content:`The text delimited by triple quotes is the name or description of the event someone is trying to get out of.  Make sure to incorporate this input into the excuse.
-      “”"${excuseType}”""` 
+        content: `The text delimited by triple quotes is the name or description of the event someone is trying to get out of.  Make sure to incorporate this input into the excuse.\n“”"${excuseType}”""` 
       }
       this.prompt.push(excuseTypePrompt)
     }
@@ -23,8 +20,7 @@ class PromptBuilder {
     if (eonetEvent) {
       let eonetPrompt = {
         role: "user",
-        content: `The text delimited by triple quotes is the name of a recent natural event. Use this natural event as PART of your excuse. For example if the input was a Volcano you could say "I can't make it because this volcano erupted". Make sure to incorporate this input into the excuse.
-        “”"${eonetEvent}”“”`
+        content: `The text delimited by triple quotes is the name of a recent natural event. Use this natural event as PART of your excuse. For example if the input was a Volcano you could say "I can't make it because this volcano erupted". Make sure to incorporate this input into the excuse.\n“”"${eonetEvent}”“”`
       }
       this.prompt.push(eonetPrompt)
     }
