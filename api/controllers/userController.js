4cf5b9b22398ab17e1a2575200c0e6b2e1ee2d83
awdem <wdemontmolling@gmail.com>
2023-08-09 16:34:05 +0100

    chore: adds framework for userController and users router

---
+++ api/controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require("../models/userModel")
+
+const UsersController = {
+    Create: () => {
+        
+    }
+};
+
+module.exports = UsersController;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const UsersController = require("../controllers/userController")
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.post("/", UsersController.Create)
 
 module.exports = router;
