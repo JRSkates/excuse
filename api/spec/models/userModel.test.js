62e2150124433ed51d00d1ee6281e15cf73bb455
awdem <wdemontmolling@gmail.com>
2023-08-09 16:26:28 +0100

    tests: adds tests for listing all users and saving a user

--- api/spec/models/userModel.test.js
+++ api/spec/models/userModel.test.js
@@ -6,7 +6,7 @@ const User = require("../../models/userModel");
 
 describe("User Model", () => {
   beforeEach((done) => {
-    console.log(mongoose.connection.collections);
+    // console.log(mongoose.connection.collections);
     mongoose.connection.collections.users.drop(() => {
       done();
     });
@@ -38,4 +38,23 @@ describe("User Model", () => {
     })
     expect(user.password).toEqual("testpassword");
   })
-})
+
+  it("can list all users", async () => {
+    const dbUsers = await User.find();
+    expect(dbUsers).toEqual([]);
+  });
+
+  it('should save a user', async () => {
+    const user = new User({
+      email: "test@example.com",
+      password: "testpassword",   
+      username: "testuser"
+    })
+    
+    await user.save()
+
+    const dbUser = await User.find()
+    console.log(dbUser[0])
+    expect(dbUser[0].email).toEqual("test@example.com");
+  })
+});
