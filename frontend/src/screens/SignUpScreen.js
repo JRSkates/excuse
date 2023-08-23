27eb8289d5787a4e097bed738fca32f6bce33e61
JRSkates <skatesproduction@gmail.com>
2023-08-23 14:13:51 +0100

    Test passing for Username TextInput

--- frontend/src/screens/SignUpScreen.js
+++ frontend/src/screens/SignUpScreen.js
@@ -5,13 +5,18 @@ const SignUp = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-}
+
 
 return(
   <View>
    <Text>Sign Up</Text>
- 
+    <TextInput 
+      placeholder="username"
+      value={username}
+      onChangeText={setUsername}
+    />
   </View>
-)
+ )
+}
 
 export default SignUp;
